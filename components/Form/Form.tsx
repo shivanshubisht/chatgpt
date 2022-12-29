dba0eac32185d828a52166e8005ce6e6f88c9712
shivanshubisht <77038395+shivanshubisht@users.noreply.github.com>
2022-12-29 16:01:56 +0530

    Add files via upload

--- components/Form/Form.tsx
+++ components/Form/Form.tsx
@@ -1,19 +1,19 @@
 'use client';
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 
 const Form = () => {
   const messageInput = useRef<HTMLTextAreaElement | null>(null);
-  const [response, setResponse] = useState<String[]>([]);
-  const [isLoading, setIsLoading] = useState<Boolean>(false);
+  const [response, setResponse] = useState<string[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handleEnter = (
-    e:
-      | React.KeyboardEvent<HTMLTextAreaElement> &
-          React.FormEvent<HTMLFormElement>
+    e: React.KeyboardEvent<HTMLTextAreaElement> &
+      React.FormEvent<HTMLFormElement>
   ) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && isLoading === false) {
       e.preventDefault();
+      setIsLoading(true);
       handleSubmit(e);
     }
   };
@@ -22,9 +22,8 @@ const Form = () => {
     e.preventDefault();
     const message = messageInput.current?.value;
     if (message !== undefined) {
-      const initialResponse: String[] = [...response, message];
+      const initialResponse: string[] = [...response, message];
       setResponse(initialResponse);
-      setIsLoading(true);
       messageInput.current!.value = '';
     }
 
@@ -33,20 +32,44 @@ const Form = () => {
     }
 
     const { data } = await axios.post('/api/response', { prompt: message });
-    const totalResponse: String[] = [...response, message, data.bot];
+    const totalResponse: string[] = [...response, message, data.bot];
     setResponse(totalResponse);
     localStorage.setItem('response', JSON.stringify(totalResponse));
     setIsLoading(false);
     messageInput.current!.value = '';
   };
 
+  const handleReset = () => {
+    localStorage.removeItem('response');
+    setResponse([]);
+  };
+
+  useEffect(() => {
+    const storedResponse = localStorage.getItem('response');
+    if (storedResponse) {
+      setResponse(JSON.parse(storedResponse));
+    }
+  }, []);
+
   return (
-    <div>
-      <div className='stretch mx-2 flex flex-col gap-3 pt-6 last:mb-6 md:mx-auto md:max-w-3xl'>
+    <div className='flex justify-center'>
+      <button
+        onClick={handleReset}
+        type='reset'
+        className='fixed top-[1.4rem] right-5 p-4 rounded-md bg-white text-gray-500 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent'
+      >
+        Clear History
+      </button>
+      <div className='w-full mx-2 flex flex-col items-start gap-3 pt-6 last:mb-6 md:mx-auto md:max-w-3xl'>
         {isLoading
           ? response.map((item: any, index: number) => {
               return (
-                <div key={index}>
+                <div
+                  key={index}
+                  className={`${
+                    index % 2 === 0 ? 'bg-blue-500' : 'bg-gray-500'
+                  } p-3 rounded-lg`}
+                >
                   <p>{item}</p>
                 </div>
               );
@@ -54,7 +77,13 @@ const Form = () => {
           : response
           ? response.map((item: any, index: number) => {
               return (
-                <div key={index}>
+                <div
+                  key={index}
+                  className={`${
+                    index % 2 === 0 ? 'bg-blue-500' : 'bg-gray-500'
+                  } p-3 rounded-lg`}
+                  // style={{ textAlign: index % 2 === 0 ? 'left' : 'right' }}
+                >
                   <p>{item}</p>
                 </div>
               );
@@ -63,34 +92,33 @@ const Form = () => {
       </div>
       <form
         onSubmit={handleSubmit}
-        className='stretch mx-2 flex flex-row gap-3 pt-6 last:mb-6 md:mx-auto md:max-w-3xl'
+        className='fixed bottom-0 w-full md:max-w-3xl bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] mb-4'
       >
-        <div className='flex flex-col w-full py-2 pl-3 flex-grow relative border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]'>
-          <textarea
-            name='Message'
-            placeholder='Type your query'
-            ref={messageInput}
-            onKeyDown={handleEnter}
-            className=' m-0 w-full resize-none border-0 bg-transparent focus:ring-0 focus-visible:ring-0 dark:bg-transparent outline-none'
-          />
-          <button
-            type='submit'
-            className='absolute top-5 p-1 rounded-md text-gray-500 right-5 hover:bg-gray-100 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent'
+        <textarea
+          name='Message'
+          placeholder='Type your query'
+          ref={messageInput}
+          onKeyDown={handleEnter}
+          className='w-full resize-none bg-transparent outline-none pt-4 pl-4 translate-y-1'
+        />
+        <button
+          disabled={isLoading}
+          type='submit'
+          className='absolute top-[1.4rem] right-5 p-1 rounded-md text-gray-500 dark:hover:text-gray-400 dark:hover:bg-gray-900 disabled:hover:bg-transparent dark:disabled:hover:bg-transparent'
+        >
+          <svg
+            stroke='currentColor'
+            fill='currentColor'
+            strokeWidth='0'
+            viewBox='0 0 20 20'
+            className='h-4 w-4 rotate-90'
+            height='1em'
+            width='1em'
+            xmlns='http://www.w3.org/2000/svg'
           >
-            <svg
-              stroke='currentColor'
-              fill='currentColor'
-              stroke-width='0'
-              viewBox='0 0 20 20'
-              className='h-4 w-4 rotate-90'
-              height='1em'
-              width='1em'
-              xmlns='http://www.w3.org/2000/svg'
-            >
-              <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
-            </svg>
-          </button>
-        </div>
+            <path d='M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z'></path>
+          </svg>
+        </button>
       </form>
     </div>
   );
--- pages/api/response.ts
+++ pages/api/response.ts
@@ -14,7 +14,7 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${prompt}`,
-      temperature: 0,
+      temperature: 0.7,
       max_tokens: 3000,
       top_p: 1,
       frequency_penalty: 0.5,
