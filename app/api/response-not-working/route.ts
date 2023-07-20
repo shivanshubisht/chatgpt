20cf25e29a2a0c7f143a8c043fd6356436c96584
shivanshu <77038395+shivanshubisht@users.noreply.github.com>
2023-07-21 04:52:33 +0530

    update encode and controller.enqueue

---
+++ app/api/response-not-working/route.ts
@@ -0,0 +1,39 @@
+import OpenAI from 'openai'
+
+const openai = new OpenAI()
+
+type RequestData = {
+  currentModel: string
+  message: string
+}
+
+export const runtime = 'edge'
+
+export async function GET(request: Request) {
+  const { message } = (await request.json()) as RequestData
+
+  if (!message) {
+    return new Response('No message in the request', { status: 400 })
+  }
+
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4',
+    messages: [{ role: 'user', content: message }],
+    stream: true,
+  })
+
+  const stream = new ReadableStream({
+    async start(controller) {
+      const encoder = new TextEncoder()
+
+      for await (const part of completion) {
+        const text = part.choices[0]?.delta.content || ''
+        const chunk = encoder.encode(text)
+        controller.enqueue(chunk)
+      }
+      controller.close()
+    },
+  })
+
+  return new Response(stream)
+}
