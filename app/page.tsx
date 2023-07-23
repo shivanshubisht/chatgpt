c45fa70ac6e592532509a52b409756eda8eed1e3
shivanshu <77038395+shivanshubisht@users.noreply.github.com>
2023-07-24 00:42:07 +0530

    parse VERCEL_URL for baseURL

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,16 @@ import type OpenAI from 'openai'
 
 const inter = Inter({ subsets: ['latin'] })
 
+function getBaseURL() {
+  if (typeof process.env.VERCEL_URL === 'string') {
+    return `https://${process.env.VERCEL_URL}`
+  }
+  return 'http://localhost:3000'
+}
+
 export default async function Home() {
   const modelsList = (await (
-    await fetch('http://localhost:3000/api/models')
+    await fetch(`${getBaseURL()}/api/models`)
   ).json()) as OpenAI.ModelsPage
   // console.log(modelsList)
   return (
